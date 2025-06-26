67bd9100abefbf2542e8df91d9f48f80c13e51c3
amberksingh <amberksingh95@gmail.com>
2025-06-26 18:36:48 +0530

    first commit

---
+++ src/main/java/com/example/Spring_cicd/SpringCicdApplication.java
@@ -0,0 +1,22 @@
+package com.example.Spring_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringCicdApplication {
+
+	@GetMapping("/welcome")
+	public String message() {
+		System.out.println("Spring ci-cd using github actions");
+		return "Spring ci-cd using github actions";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCicdApplication.class, args);
+	}
+
+}
