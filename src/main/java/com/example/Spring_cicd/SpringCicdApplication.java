0b4a8e3fbbd0754b98a295e0c86d7d1f28eb2732
amberksingh <amberksingh95@gmail.com>
2025-08-01 20:56:15 +0530

    making argocd and pipeline dynamic only for src and pom change

--- src/main/java/com/example/Spring_cicd/SpringCicdApplication.java
+++ src/main/java/com/example/Spring_cicd/SpringCicdApplication.java
@@ -11,8 +11,8 @@ public class SpringCicdApplication {
 
 	@GetMapping("/welcome")
 	public String message() {
-		System.out.println("Spring ..ci-cd using github actions..august!");
-		return "Spring ci-cd using github actions..!august";
+		System.out.println("Spring ..ci-cd using github actions..AUGUST!");
+		return "Spring ci-cd using github actions..!AUGUST";
 	}
 
 	public static void main(String[] args) {
