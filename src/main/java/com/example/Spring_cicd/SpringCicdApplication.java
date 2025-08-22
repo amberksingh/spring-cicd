c8afce2687ee40ef7ea5c261a7be1149269cd505
amberksingh <amberksingh95@gmail.com>
2025-08-22 20:26:41 +0530

    changed replicas for java file change argocd and endpoint test

--- src/main/java/com/example/Spring_cicd/SpringCicdApplication.java
+++ src/main/java/com/example/Spring_cicd/SpringCicdApplication.java
@@ -11,8 +11,8 @@ public class SpringCicdApplication {
 
 	@GetMapping("/welcome")
 	public String message() {
-		System.out.println("Spring ..ci-cd using github actions..AUGUST end..!");
-		return "Spring ci-cd using github actions..!AUGUST end..";
+		System.out.println("Spring ..ci-cd using github actions..AUGUST end..!!!");
+		return "Spring ci-cd using github actions..!AUGUST end..!!!";
 	}
 
 	public static void main(String[] args) {
